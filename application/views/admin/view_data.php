77f2271863b6bf5bead090f8ce7cfcf0cc84a412
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2019-11-21 11:45:04 +0700

    Update Responsive Table

--- application/views/admin/view_data.php
+++ application/views/admin/view_data.php
@@ -14,71 +14,73 @@
           </div>
         </div>
         <div class="box-body">
-          <table class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th style="width: 10px">#</th>
-                    <th>Kode</th>
-                    <th>List Share</th>
-                    <th>Market Value</th>
-                    <th>Assets</th>
-                    <th>Piutang</th>
-                    <th>Hutang</th>
-                    <th>Modal</th>
-                    <th>Pendapatan</th>
-                    <th>EPS</th>
-                    <th>ROA</th>
-                    <th>ROE</th>
-                    <th>DAR</th>
-                    <th>DER</th>
-                    <th>Tobin's Q</th>
-                    <th>PBV</th>
-                    <th>Closing Price</th>
-                    <th style="width: 10%">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php  
-                    if (!$data->count()) {
-                      echo "<tr><td colspan='9' class='text-center'>Empty</td></tr>";
-                    } else {
-                      $no = 1;
-                      foreach ($data as $value) {
-                  ?>
-                  <tr>
-                    <td><?= $no ?>.</td>
-                    <td><?= $value->kode ?></td>
-                    <td>Rp. <?= number_format($value->list_share, 2) ?></td>
-                    <td>Rp. <?= number_format($value->closing_price*$value->list_share, 2) ?></td>
-                    <td>Rp. <?= number_format($value->assets, 2) ?></td>
-                    <td>Rp. <?= number_format($value->piutang, 2) ?></td>
-                    <td>Rp. <?= number_format($value->hutang, 2) ?></td>
-                    <td>Rp. <?= number_format($value->modal, 2) ?></td>
-                    <td>Rp. <?= number_format($value->pendapatan, 2) ?></td>
-                    <td>Rp. <?= number_format($value->eps, 2) ?></td>
-                    <td><?= number_format($value->roa, 2) ?> %</td>
-                    <td><?= number_format($value->roe, 2) ?> %</td>
-                    <td><?= number_format($value->dar, 2) ?></td>
-                    <td><?= number_format($value->der, 2) ?></td>
-                    <td><?= round(($value->closing_price*$value->list_share) / $value->assets, 3) ?></td>
-                    <td>Rp. <?= number_format($value->pbv, 2) ?></td>
-                    <td>Rp. <?= number_format($value->closing_price, 2) ?></td>
-                    <td>
-                      <a href="<?= base_url() ?>index.php/admin/edittobinsq/<?= $value->id ?>">
-                        <span class="badge bg-blue">Edit</span>
-                      </a>
-                      <a href="<?= base_url() ?>index.php/tobinsq_c/delete/<?= $value->id ?>">
-                        <span class="badge bg-red">Delete</span> 
-                      </a>
-                    </td>
-                  </tr>
-                  <?php  
-                        $no++;
-                      } // foreach    
-                    } // else
-                  ?>
-                </tbody>
-              </table>
+          <div class="table-responsive">
+            <table class="table table-bordered" >
+                  <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Kode</th>
+                      <th>List Share</th>
+                      <th>Market Value</th>
+                      <th>Assets</th>
+                      <th>Piutang</th>
+                      <th>Hutang</th>
+                      <th>Modal</th>
+                      <th>Pendapatan</th>
+                      <th>EPS</th>
+                      <th>ROA</th>
+                      <th>ROE</th>
+                      <th>DAR</th>
+                      <th>DER</th>
+                      <th>Tobin's Q</th>
+                      <th>PBV</th>
+                      <th>Closing Price</th>
+                      <th style="width: 10%">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php  
+                      if (!$data->count()) {
+                        echo "<tr><td colspan='9' class='text-center'>Empty</td></tr>";
+                      } else {
+                        $no = 1;
+                        foreach ($data as $value) {
+                    ?>
+                    <tr>
+                      <td><?= $no ?>.</td>
+                      <td><?= $value->kode ?></td>
+                      <td>Rp. <?= number_format($value->list_share, 2) ?></td>
+                      <td>Rp. <?= number_format($value->closing_price*$value->list_share, 2) ?></td>
+                      <td>Rp. <?= number_format($value->assets, 2) ?></td>
+                      <td>Rp. <?= number_format($value->piutang, 2) ?></td>
+                      <td>Rp. <?= number_format($value->hutang, 2) ?></td>
+                      <td>Rp. <?= number_format($value->modal, 2) ?></td>
+                      <td>Rp. <?= number_format($value->pendapatan, 2) ?></td>
+                      <td>Rp. <?= number_format($value->eps, 2) ?></td>
+                      <td><?= number_format($value->roa, 2) ?> %</td>
+                      <td><?= number_format($value->roe, 2) ?> %</td>
+                      <td><?= number_format($value->dar, 2) ?></td>
+                      <td><?= number_format($value->der, 2) ?></td>
+                      <td><?= round(($value->closing_price*$value->list_share) / $value->assets, 3) ?></td>
+                      <td>Rp. <?= number_format($value->pbv, 2) ?></td>
+                      <td>Rp. <?= number_format($value->closing_price, 2) ?></td>
+                      <td>
+                        <a href="<?= base_url() ?>index.php/admin/edittobinsq/<?= $value->id ?>">
+                          <span class="badge bg-blue">Edit</span>
+                        </a>
+                        <a href="<?= base_url() ?>index.php/tobinsq_c/delete/<?= $value->id ?>">
+                          <span class="badge bg-red">Delete</span> 
+                        </a>
+                      </td>
+                    </tr>
+                    <?php  
+                          $no++;
+                        } // foreach    
+                      } // else
+                    ?>
+                  </tbody>
+                </table>
+          </div>
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
